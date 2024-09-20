e613967c26d21b7406cbd6400140807679b599c5
Kenti Antezana <mopup2017@gmail.com>
2024-09-20 13:32:48 -0300

    Feat: Agrego Landing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import {MainLayoutComponent} from "./shared/main-layout/main-layout.component";
 import {LoginComponent} from "./features/auth/login/login.component";
 import {RegisterComponent} from "./features/auth/register/register.component";
+import {HomeComponent} from "./features/home/home/home.component";
 
 export const routes: Routes = [
   {
     path:'',
     component:MainLayoutComponent,
     children:[
+      {path:'', component:HomeComponent},
       {path:'login',component:LoginComponent},
       {path:'register',component:RegisterComponent}
     ]
