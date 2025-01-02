ef32d5d40d5b01c84c1fdd7bd5e40295906a73b6
Kenti Antezana <mopup2017@gmail.com>
2025-01-02 12:32:03 -0300

    Add database renewal workflow

--- backend/src/main/java/com/myfitnessapp/config/SecurityConfig.java
+++ backend/src/main/java/com/myfitnessapp/config/SecurityConfig.java
@@ -7,14 +7,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Value("${frontend.baseUrl}")
     private String frontendUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
 
     @Bean
     public CorsFilter corsFilter() {
